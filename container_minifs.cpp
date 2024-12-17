740a46ddcccc9c4dcb9acb013fdeaf3989b57715
MarlzRana <77016115+MarlzRana@users.noreply.github.com>
2024-12-17 12:55:04 +0000

    Remove Unneeded Comments

--- container_minifs.cpp
+++ container_minifs.cpp
@@ -10,10 +10,6 @@
 #include <sys/wait.h>
 #include <format>
 
-// constexpr std::string_view ALPINE_LINUX_MINIFS_URL{"https://dl-cdn.alpinelinux.org/alpine/v3.20/releases/aarch64/alpine-minirootfs-3.20.3-aarch64.tar.gz"};
-// const std::filesystem::path CONTAINER_OVERLAY_FS{"/tmp/my-container-runtime"};
-// const std::filesystem::path CONTAINER_ROOT{"/tmp/my-container-runtime/merged"};
-
 void makeContainerOverlayFSDirectories() {
     // Make the directories that will make up the overlay file system
     std::filesystem::create_directories(CONTAINER_OVERLAY_FS_BASE);
